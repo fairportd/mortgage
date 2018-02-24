a775097abdcd1cf036dd5cd1a52a268264c2191f
fairportd <daveandrews42@gmail.com>
2018-02-23 20:47:11 -0500

    adjusted T & I down to 3.3% from 3.6% to adjust for school taxes

--- mortgage_all_in.py
+++ mortgage_all_in.py
@@ -14,7 +14,7 @@ import numpy as np
 def rate_table(rates):
     for rate in rates:
             all_in_m = (np.pmt(rate / 100 / 12, 360, -100000) * 12) / 100000
-            tandi = 0.046 # assumes 3.6% tax and 1% insurance
+            tandi = 0.043 # assumes 3.3% tax and 1% insurance
             total = all_in_m + tandi
             print('{0:.2f}%  {1:.2f}%  {2:.2f}x  {3:.2f}%  {4:.2f}x'.format(rate, all_in_m*100, all_in_m*100 / (rate), (total)*100, (total)*100 / (rate) ))
 
@@ -22,14 +22,14 @@ def rate_table(rates):
 
 def rent_eqv(rent):
     a_rent = rent * 12 # annualize rent payments
-    cap_rate = .1033 # all in PITI % at 4% for 30, with 3% in T & I
+    cap_rate = .1003 # all in PITI % at 4% for 30, with 3% in T & I
     eqv_loan = a_rent / cap_rate # calc pv of loan with total payments equal to rent payment
     price = eqv_loan / 0.80 # assume 20% down payment (80% LTV)
     print("You're current rent of ${0:,.0f} is equivelant to PITI payments on a ${1:,.0f} house.".format(rent, price))
 
 def afford(income, dti):
     a_pmts = income * dti/100
-    cap_rate = .1033 # all in rate at 4% for 30 with 3.6% fairport taxes and 1% insurance
+    cap_rate = .1003 # all in rate at 4% for 30 with 3.6% fairport taxes and 1% insurance
     value = a_pmts / cap_rate
     print('With an income of ${0:,.0f} you can afford a ${1:,.0f} house with a {2:.0f}% DTI ratio'.format(income, value, dti))
 
@@ -43,3 +43,6 @@ for rent in rents:
 
 for income in incomes:
     afford(income, 20)
+
+actual_rent = 1035
+rent_eqv(actual_rent)
